8143aadd8e6e53529e37b124b216b1e504a7135d
Jhon Munoz <jhon.munoz@teaminternational.com>
2022-10-05 10:45:28 -0500

    new endpoint

--- movie/movie.py
+++ movie/movie.py
@@ -1,4 +1,5 @@
-from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
+from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, \
+    QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, request, jsonify
 
@@ -10,12 +11,28 @@ app = Flask(__name__)
 
 ### ADD THINGS HERE
 #
+type_defs = load_schema_from_path('movie.graphql')
+query = QueryType()
+movie = ObjectType('Movie')
+query.set_field('movie_with_id', r.movie_with_id)
+
+mutation = MutationType()
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+
+actor = ObjectType('Actor')
+movie.set_field('actors', r.resolve_actors_in_movie)
+query.set_field('actor_with_id', r.actor_with_id)
+
+query.set_field('all_movies', r.all_movies)
+
+schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 ###
 
 # root message
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
+
 
 #####
 # graphql entry points
@@ -23,19 +40,21 @@ def home():
 @app.route('/graphql', methods=['GET'])
 def playground():
     return PLAYGROUND_HTML, 200
-    
+
+
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
     data = request.get_json()
     success, result = graphql_sync(
-    schema,
-    data,
-    context_value=None,
-    debug=app.debug
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
     )
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
+
 if __name__ == "__main__":
-    print("Server running in port %s"%(PORT))
+    print("Server running in port %s" % (PORT))
     app.run(host=HOST, port=PORT)

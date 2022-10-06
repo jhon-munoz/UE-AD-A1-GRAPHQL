6355c9df3bce1e45809690c7bfc59aea6b5f2e0f
Gustavo Soares <gusnsoares@gmail.com>
2022-10-06 13:54:21 +0200

    movie

--- movie/movie.py
+++ movie/movie.py
@@ -1,8 +1,7 @@
-from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, \
-    QueryType, MutationType
+from ariadne import (gql, graphql_sync, make_executable_schema,
+                     load_schema_from_path, ObjectType, QueryType, MutationType)
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
-
+from flask import Flask, request, jsonify, make_response
 import resolvers as r
 
 PORT = 3001
@@ -32,14 +31,17 @@ query.set_field('movies_by_rating', r.movies_by_rating)
 schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 ###
 
+
 # root message
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
+    return make_response(
+        "<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
 
 
 #####
 # graphql entry points
+
 
 @app.route('/graphql', methods=['GET'])
 def playground():
@@ -49,12 +51,10 @@ def playground():
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
     data = request.get_json()
-    success, result = graphql_sync(
-        schema,
-        data,
-        context_value=None,
-        debug=app.debug
-    )
+    success, result = graphql_sync(schema,
+                                   data,
+                                   context_value=None,
+                                   debug=app.debug)
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
